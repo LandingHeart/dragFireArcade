e17a6077b39e83a577ff6c253cae53044c5cc575
Shinan Liu <Shinan.liu@yahoo.com>
2021-01-06 20:36:48 -0500

    all files

---
+++ Assets/TrajectorLine.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrajectorLine : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+    private void Awake() {
+        lineRenderer = GetComponent<LineRenderer>();    
+    }
+    public void RenderLine(Vector3 startPoint, Vector2 endPoint){
+        lineRenderer.positionCount = 2;
+        Vector3[] points = new Vector3[2];
+        points[0] = startPoint;
+        points[1] = endPoint;
+        
+        lineRenderer.SetPositions(points);
+
+    }
+    public void EndLine(){
+        lineRenderer.positionCount = 0;
+    }
+}
